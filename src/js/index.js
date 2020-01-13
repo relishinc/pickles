da744675a33aff905a183a7118d6e7eb5952c7ba
Steve Palmer <steve@reli.sh>
2020-01-12 21:56:44 -0800

    no message

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,20 @@
-// index
+// ui
 
 import Modal from './src/js/ui/modal';
 import Lightbox from './src/js/ui/lightbox';
 import Drawer from './src/js/ui/drawer';
 
+// anim
+
 import ScrollEffects from './src/js/anim/scroll-effects';
+
+// utils
 
 import AjaxForms from './src/js/utils/ajax-forms';
 import FocusTrap from './src/js/utils/focus-trap';
 import ScrollListener from './src/js/utils/scroll-listener';
+
+// export
 
 export {
   Modal,
